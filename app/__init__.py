19fce9055cf5d160e82efa033f05bdb67afb9097
Alek <alek@spectra-alarm.com>
2022-05-26 23:22:23 +0200

    created new user

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ app.config['JSON_AS_ASCII'] = False
 
 host = getattr(environ, 'FLASK_DATABASE_HOST', 'localhost')
 con = pymongo.MongoClient(f"mongodb://root:password@{host}:27017/")
-db = con['probadb']
-print(con.list_database_names())
-    
+users_col = con['users']['users']
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
-from app import app, db
+from app import app, users_col
+from app.models import User
 
 @app.route('/')
 def main():
@@ -10,9 +11,24 @@ def main():
 def test_db():
     print('connecting')
     users_arr = []
-    users = db['users'].find()
+    users = users_col.find()
     for user in users:
         print(user)
         users_arr.append(user)
     
     return jsonify(users_arr)
+
+@app.post('/create-new-user')
+def create_new_user():
+    data = request.json
+    print(f'data: {data}, {type(data)}')
+    user = User(name=data['name'], 
+                surname=data['surname'], 
+                email=data['email']
+                )
+
+    # add to db
+    response = users_col.insert_one(data)
+    print(response.acknowledged, response.inserted_id)
+    return jsonify("done")
+

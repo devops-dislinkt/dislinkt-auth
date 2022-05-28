e307b4a90d3dbccde13c1e48f9c9488406dbff2b
Alek <alek@spectra-alarm.com>
2022-05-28 12:24:12 +0200

    hot fix in env vars

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ import pymongo
 
 environ['FLASK_ENV'] = 'development'
 app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-app.config['SECRET_KEY'] = getattr(environ, 'FLASK_SECRET_KEY', 'mysecretkey')
 
-host = getattr(environ, 'FLASK_DATABASE_HOST', 'localhost')
-con = pymongo.MongoClient(f"mongodb://root:password@{host}:27017/")
+app.config['JSON_AS_ASCII'] = False
+app.config['SECRET_KEY'] = environ['FLASK_SECRET_KEY'] if environ['FLASK_SECRET_KEY'] else 'my_secret_key'
+app.config['HOST']  = environ['FLASK_DATABASE_HOST'] if environ['FLASK_DATABASE_HOST'] else 'localhost'
+
+con = pymongo.MongoClient(f"mongodb://root:password@{app.config['HOST']}:27017/")
 users_col = con['users']['users']
 
 from app import routes

4638f065519104dd177ffa35f7d3012700f30e4c
Alek <alek@spectra-alarm.com>
2022-05-31 19:23:22 +0200

    todo za kafku

--- app/routes.py
+++ app/routes.py
@@ -31,7 +31,11 @@ def create_new_user():
             '_id': user.username, 
             'password': user.password,
             'role': user.role})
-        # producer.send(current_app.config['KAFKA_TOPIC'], {'username': user.username})
+        
+        # TODO: podesiti kafku da posalje test topic ako je test env. 
+        # Mislim da bi trebalo preuzeti vrednost nakon send funkcije i proveriti da li se send izvrsio.
+        # Ako se send nije izvrsio treba da uhvatimo neki exception
+        producer.send(current_app.config['KAFKA_TOPIC'], {'username': user.username})
         
     except DuplicateKeyError:
         return jsonify("username not unique"), 400
